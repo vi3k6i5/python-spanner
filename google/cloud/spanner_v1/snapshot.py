0e90dd1c85fbeddd615289097e673d7692636eae
Vikash Singh <3116482+vi3k6i5@users.noreply.github.com>
2021-03-25 11:26:04 +0530

    feat: lint fix

--- google/cloud/spanner_v1/snapshot.py
+++ google/cloud/spanner_v1/snapshot.py
@@ -408,10 +408,7 @@ class _SnapshotBase(_SessionWrapper):
             "CloudSpanner.PartitionReadOnlyTransaction", self._session, trace_attributes
         ):
             response = api.partition_read(
-                request=request,
-                metadata=metadata,
-                retry=retry,
-                timeout=timeout,
+                request=request, metadata=metadata, retry=retry, timeout=timeout,
             )
 
         return [partition.partition_token for partition in response.partitions]
@@ -503,10 +500,7 @@ class _SnapshotBase(_SessionWrapper):
             trace_attributes,
         ):
             response = api.partition_query(
-                request=request,
-                metadata=metadata,
-                retry=retry,
-                timeout=timeout,
+                request=request, metadata=metadata, retry=retry, timeout=timeout,
             )
 
         return [partition.partition_token for partition in response.partitions]
